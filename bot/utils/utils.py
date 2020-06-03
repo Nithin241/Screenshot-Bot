2ecac7243d8e3bc8b1dbe2773e6b028a9b0d6320
Nithin241 <64920127+Nithin241@users.noreply.github.com>
2020-06-03 11:28:34 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -403,7 +403,7 @@ async def sample_fn(c, m):
                 supports_streaming=True
             )
         
-        await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\n\n\n©️ @Nithinofficial_admin  @NRBotsZ ')
+        await edit_message_text(m, text=f'Successfully completed process in {datetime.timedelta(seconds=int(time.time()-start_time))}\n\n\n\n©️ @Nithinofficial_admin  @NRBotsZ  @Kannadawarriors , @Nithinmovies ')
         c.CURRENT_PROCESSES[chat_id] -= 1
         
     except:
