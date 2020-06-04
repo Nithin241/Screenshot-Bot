03bda47605aab624513069c8f9e068b6266b19dd
Nithin241 <64920127+Nithin241@users.noreply.github.com>
2020-06-04 16:47:21 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -16,7 +16,7 @@ async def start(c, m):
     
     await m.reply_text(
         text=f"Hi there {m.from_user.first_name}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from your video files with out downloading the entire file (almost instantly). For more details check /help.",
-If you like you can join our another projects channel @NRBotsZ
+
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
